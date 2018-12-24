2fc859db512cd4bab77b64a5573f950984a2c531
Haque <34745319+HaqueMannan@users.noreply.github.com>
2018-12-24 21:45:49 +0000

    Section 2 - Diving Into Components - 2.2. Properties & Methods.

---
+++ Section 2 - Diving Into Components/Section 2.2 - Properties & Methods/Angular-Sandbox/src/app/components/user/user.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component ({
+   selector: 'app-user',
+   templateUrl: './user.component.html',
+   styleUrls: ['./user.component.css']
+})
+
+export class UserComponent {
+   // Properties:
+   firstName = 'John';
+   lastName = 'Doe';
+   age = 28;
+
+   // Methods:
+   // Constructors automatically runs when the component instantiates/initiates.
+   constructor() {
+      console.log('Hello user...');
+      this.sayHello();
+      console.log(this.age);
+      this.hasBirthday();
+      console.log(this.age);
+   }
+   // Own customer method:
+   sayHello() {
+      console.log(`Hello ${this.firstName}`);
+   }
+   hasBirthday() {
+      this.age += 1;
+   }
+}
