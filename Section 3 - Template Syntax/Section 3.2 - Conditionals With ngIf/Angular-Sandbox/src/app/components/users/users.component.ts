7d6bca6c8df64e677ddf7b88f217fb7eaf0583ef
Haque <34745319+HaqueMannan@users.noreply.github.com>
2018-12-27 00:18:12 +0000

    Section 3 - Template Syntax - 3.2. Conditionals With ngIf.

---
+++ Section 3 - Template Syntax/Section 3.2 - Conditionals With ngIf/Angular-Sandbox/src/app/components/users/users.component.ts
@@ -0,0 +1,79 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/User';
+
+@Component({
+   selector: 'app-users',
+   templateUrl: './users.component.html',
+   styleUrls: ['./users.component.css']
+})
+
+export class UsersComponent implements OnInit {
+   users: User[];
+   showExtended: boolean = true;
+   loaded: boolean = false;
+
+   constructor() { }
+
+   ngOnInit() {
+      setTimeout(() => {
+         this.users = [
+            {
+               firstName: 'John',
+               lastName: 'Doe',
+               age: 28,
+               address: {
+                  street: '1 Abbey Street',
+                  city: 'Birmingham',
+                  country: 'UK'
+               }
+            },
+            {
+               firstName: 'Kathy',
+               lastName: 'Blyth',
+               age: 18,
+               address: {
+                  street: '20 Barkby Road',
+                  city: 'Coventry',
+                  country: 'UK'
+               }
+            },
+            {
+               firstName: 'Larry',
+               lastName: 'Long',
+               age: 35,
+               address: {
+                  street: '33 Coalville Avenue',
+                  city: 'Dublin',
+                  country: 'UK'
+               }
+            }
+         ];
+
+         this.loaded = true;
+
+         this.addUser({
+            firstName: 'Elena',
+            lastName: 'Gilbert',
+         })
+      }, 3000);
+
+      // Show/Hide the extended user information:
+      // this.showExtended = false;
+
+      // this.addUser({
+      //    firstName: 'Elena',
+      //    lastName: 'Gilbert',
+      //    age: 30,
+      //    address: {
+      //       street: '33 Dunkirk Road',
+      //       city: 'Edinburgh',
+      //       country: 'UK'
+      //    }
+      // });
+   }
+
+   // Push method is part of JavaScript which adds an object onto an array.
+   addUser(user: User) {
+      this.users.push(user);
+   }
+}
---
+++ Section 3 - Template Syntax/Section 3.2 - Conditionals With ngIf/Angular-Sandbox/src/app/models/User.ts
@@ -0,0 +1,13 @@
+// Interface - model User data:
+export interface User {
+   firstName: string,
+   lastName: string,
+   age?: number,
+   address?: {
+      street?: string,
+      city?: string,
+      country?: string
+   }
+}
+
+// Adding ? next to the property name(s) makes these properties optional.
