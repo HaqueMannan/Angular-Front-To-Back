b9a08078b6ab1e5da6618e064c6b6e2e108dc319
Haque <34745319+HaqueMannan@users.noreply.github.com>
2018-12-30 01:29:03 +0000

    Section 4 - Events & Forms - 4.3. Form Input & Keyboard Events.

---
+++ Section 4 - Events & Forms/Section 4.3 - Form Input Events/Angular-Sandbox/src/app/components/users/users.component.ts
@@ -0,0 +1,87 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/User';
+
+@Component({
+   selector: 'app-users',
+   templateUrl: './users.component.html',
+   styleUrls: ['./users.component.css']
+})
+
+export class UsersComponent implements OnInit {
+   users: User[];
+   showExtended: boolean = true;
+   loaded: boolean = false;
+   enableAdd: boolean = true;
+   showUserForm: boolean = false;
+
+   constructor() { }
+
+   ngOnInit() {
+      this.users = [
+         {
+            firstName: 'John',
+            lastName: 'Doe',
+            age: 28,
+            address: {
+               street: '1 Abbey Street',
+               city: 'Birmingham',
+               country: 'UK'
+            },
+            isActive: true,
+            registered: new Date('01/02/2018 08:30:00'),
+            hide: true
+         },
+         {
+            firstName: 'Kathy',
+            lastName: 'Blyth',
+            age: 18,
+            address: {
+               street: '20 Barkby Road',
+               city: 'Coventry',
+               country: 'UK'
+            },
+            isActive: false,
+            registered: new Date('07/15/2018 18:00:00'),
+            hide: true
+         },
+         {
+            firstName: 'Larry',
+            lastName: 'Long',
+            age: 35,
+            address: {
+               street: '33 Coalville Avenue',
+               city: 'Dublin',
+               country: 'UK'
+            },
+            isActive: true,
+            registered: new Date('10/10/2017 10:55:00'),
+            hide: true
+         }
+      ];
+
+      this.loaded = true;
+   }
+
+   // Push method is part of JavaScript which adds an object onto an array.
+   addUser(user: User) {
+      this.users.push(user);
+   }
+
+   // Toggle show/hide user details. Note: this function expression can be added directly in the template file click event rather than calling this function.
+   // toggleHide(user: User) {
+   //    user.hide = !user.hide;
+   // }
+
+   // Form Submit function.
+   onSubmit(e) {
+      e.preventDefault();
+      console.log(123);
+   }
+
+   // Form input event displays the key pressed on the keyboard.
+   fireEvent(e) {
+      console.log(e);
+      console.log(e.type);
+      console.log(e.target.value);
+   }
+}
