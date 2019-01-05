c5b44eeb658e8bff4be88ad6873d24e5e80fe644
Haque <34745319+HaqueMannan@users.noreply.github.com>
2019-01-05 02:15:26 +0000

    Section 5 - Services, HTTP, Input & Output - 5.4. HTTP Client POST Request.

---
+++ Section 5 - Services, HTTP, Input & Output/Section 5.4 - HTTP Client POST Request/Angular-Sandbox/src/app/components/post-form/post-form.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { PostService } from '../../services/post.service';
+import { Post } from '../../models/Post';
+
+@Component({
+   selector: 'app-post-form',
+   templateUrl: './post-form.component.html',
+   styleUrls: ['./post-form.component.css']
+})
+export class PostFormComponent implements OnInit {
+
+   constructor(private _postService: PostService) { }
+
+   ngOnInit() {
+   }
+
+   addPost(title, body) {
+      if(!title || !body) {
+         alert('Please add post!')
+      } else {
+         // console.log(title, body);
+         this._postService.savePost({title, body} as Post).subscribe(post => {
+            console.log(post);
+         });
+      }
+   }
+}
