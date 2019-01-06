cbc4e16630704d7c40396045438de6ff1b4251cf
Haque <34745319+HaqueMannan@users.noreply.github.com>
2019-01-06 16:24:52 +0000

    Section 6 - Angular Router - 6.2. Creating & Mapping Routes.

---
+++ Section 6 - Angular Router/Section 6.2 - Creating & Mapping Routes/Angular-Sandbox/src/app/components/post-form/post-form.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
+import { PostService } from '../../services/post.service';
+import { Post } from '../../models/Post';
+
+@Component({
+   selector: 'app-post-form',
+   templateUrl: './post-form.component.html',
+   styleUrls: ['./post-form.component.css']
+})
+export class PostFormComponent implements OnInit {
+   @Output() newPost: EventEmitter<Post> = new EventEmitter;
+   @Output() updatedPost: EventEmitter<Post> = new EventEmitter;
+   @Input() currentPost: Post;
+   @Input() isEdit: boolean;
+
+   constructor(private _postService: PostService) { }
+
+   ngOnInit() {
+   }
+
+   addPost(title, body) {
+      if(!title || !body) {
+         alert('Please add post!')
+      } else {
+         // console.log(title, body);
+         this._postService.savePost({title, body} as Post).subscribe(post => {
+            this.newPost.emit(post);
+         });
+      }
+   }
+
+   // Output the updatedPost so that the value can be used in the post.component file to update the posts list and reset the isEdit property back to false and clear the form.
+   updatePost() {
+      this._postService.updatePost(this.currentPost).subscribe(post => {
+         this.updatedPost.emit(post);
+      });
+   }
+}
---
+++ Section 6 - Angular Router/Section 6.2 - Creating & Mapping Routes/Angular-Sandbox/src/app/components/posts/posts.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from '@angular/core';
+import { PostService } from '../../services/post.service';
+import { Post } from '../../models/Post';
+
+@Component({
+   selector: 'app-posts',
+   templateUrl: './posts.component.html',
+   styleUrls: ['./posts.component.css']
+})
+export class PostsComponent implements OnInit {
+   posts: Post[];
+   currentPost: Post = {
+      id: 0,
+      title: '',
+      body: ''
+   }
+   isEdit: boolean = false;
+
+   constructor(private _postService: PostService) { }
+
+   ngOnInit() {
+      this._postService.getPosts().subscribe(posts => {
+         console.log(posts);
+         this.posts = posts;
+      });
+   }
+
+   onNewPost(post: Post) {
+      this.posts.unshift(post);
+      this.currentPost = {
+         id: 0,
+         title: '',
+         body: ''
+      };
+   }
+
+   editPost(post: Post) {
+      this.currentPost = post;
+      this.isEdit = true;
+   }
+
+   // Loop through posts array property to find the updated post and .splice() to remove the post from the array and then .unshift() to add the new updated post to the top of the array list. Finally, set the isEdit property to false.
+   onUpdatedPost(post: Post) {
+      this.posts.forEach((curr, index) => {
+         if(post.id === curr.id) {
+            this.posts.splice(index, 1);
+            this.posts.unshift(post);
+            this.isEdit = false;
+            this.currentPost = {
+               id: 0,
+               title: '',
+               body: ''
+            };
+         };
+      });
+   }
+
+   removePost(post: Post) {
+      if(confirm('Are you sure?')) {
+         this._postService.removePost(post.id).subscribe(() => {
+            this.posts.forEach((curr, index) => {
+               if(post.id === curr.id) {
+                  this.posts.splice(index, 1);
+               };
+            });
+         });
+      };
+   }
+}
