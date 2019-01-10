5997bb57f6f49f7fa78f2c7cb5dda275afe4f1fa
Haque <34745319+HaqueMannan@users.noreply.github.com>
2019-01-10 21:37:51 +0000

    Section 7 - DevLogger Project - 7.1. Components & Bootstrap Setup.

---
+++ Section 7 - Project  - DevLogger/Section 7.1 - Components & Bootstrap Setup/devlogger/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { LogFormComponent } from './components/log-form/log-form.component';
+import { LogsComponent } from './components/logs/logs.component';
+
+@NgModule({
+   declarations: [AppComponent, NavbarComponent, LogFormComponent, LogsComponent],
+   imports: [BrowserModule, AppRoutingModule],
+   providers: [],
+   bootstrap: [AppComponent]
+})
+export class AppModule {}
