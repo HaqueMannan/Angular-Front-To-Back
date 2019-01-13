26cbc222ad85c6b41d18b606c92df167af29236a
Haque <34745319+HaqueMannan@users.noreply.github.com>
2019-01-13 16:06:08 +0000

    Section 8 - Client Panel Project - 8.3. AngularFire2 Setup & Client Service.

---
+++ Section 8 - Project - Client Panel/Section 8.3 - AngularFire2 Setup & Client Service/clientpanel/src/app/components/clients/clients.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { ClientService } from '../../services/client.service';
+import { Client } from '../../models/Client';
+
+@Component({
+   selector: 'app-clients',
+   templateUrl: './clients.component.html',
+   styleUrls: ['./clients.component.css']
+})
+export class ClientsComponent implements OnInit {
+   clients: Client[];
+
+   constructor(private _clientService: ClientService) { }
+
+   ngOnInit() {
+      this._clientService.getClients().subscribe(clients => this.clients = clients);
+   }
+}
