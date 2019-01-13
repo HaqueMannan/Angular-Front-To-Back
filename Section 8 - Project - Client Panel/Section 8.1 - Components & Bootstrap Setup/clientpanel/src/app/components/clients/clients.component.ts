5726c73e376e9a7e916c806f97ef3cdb1cd54e16
Haque <34745319+HaqueMannan@users.noreply.github.com>
2019-01-13 14:01:21 +0000

    Section 8 - Client Panel Project - 8.1. Components & Bootstrap Setup.

---
+++ Section 8 - Project - Client Panel/Section 8.1 - Components & Bootstrap Setup/clientpanel/src/app/components/clients/clients.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+   selector: 'app-clients',
+   templateUrl: './clients.component.html',
+   styleUrls: ['./clients.component.css']
+})
+export class ClientsComponent implements OnInit {
+
+   constructor() { }
+
+   ngOnInit() {
+   }
+}
