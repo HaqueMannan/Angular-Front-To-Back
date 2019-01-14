21367a5fed0e16b490b0d20bfa216213eaddaed7
Haque <34745319+HaqueMannan@users.noreply.github.com>
2019-01-14 21:04:41 +0000

    Section 8 - Client Panel Project - 8.7. Client Details Component.

---
+++ Section 8 - Project - Client Panel/Section 8.7 - Client Details Component/clientpanel/src/app/components/client-details/client-details.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { ClientService } from '../../services/client.service';
+import { Client } from '../../models/Client';
+
+@Component({
+   selector: 'app-client-details',
+   templateUrl: './client-details.component.html',
+   styleUrls: ['./client-details.component.css']
+})
+export class ClientDetailsComponent implements OnInit {
+   id: string;
+   client: Client;
+   hasBalance: boolean = false;
+   showBalaceUpdateInput: boolean = false;
+
+   constructor(
+      private _router: Router,
+      private _route: ActivatedRoute,
+      private _flashMessage: FlashMessagesService,
+      private _clientService: ClientService
+   ) { }
+
+   ngOnInit() {
+      // Get id from URL
+      this.id = this._route.snapshot.params['id'];
+      // Get Client data
+      this._clientService.getClient(this.id).subscribe(client => {
+         // if(client != null) {
+         //    if(client.balance > 0) {
+         //       this.hasBalance = true;
+         //    }
+         // }
+         this.client = client;
+      });
+   }
+}
