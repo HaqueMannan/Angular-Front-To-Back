030c60c50b9878d055fdee2b155ce6d744ab50a5
Haque <34745319+HaqueMannan@users.noreply.github.com>
2019-01-14 20:09:48 +0000

    Section 8 - Client Panel Project - 8.6. Add Client to Firestore.

---
+++ Section 8 - Project - Client Panel/Section 8.6 - Add Client to Firestore/clientpanel/src/app/services/client.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { of } from "rxjs"
+import { map } from "rxjs/operators"
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+
+import { Client } from '../models/Client';
+
+@Injectable({
+   providedIn: 'root'
+})
+export class ClientService {
+   clientsCollection: AngularFirestoreCollection<Client>;
+   clientDoc: AngularFirestoreDocument<Client>;
+   clients: Observable<Client[]>;
+   client: Observable<Client>;
+
+   constructor(private _afs: AngularFirestore) {
+      this.clientsCollection = this._afs.collection('clients', ref => ref.orderBy('lastName', 'asc'));
+   }
+
+   getClients(): Observable<Client[]> {
+      // Get client with the id
+      this.clients = this.clientsCollection.snapshotChanges().pipe(
+         map(changes => changes.map(action => {
+            const data = action.payload.doc.data() as Client;
+            data.id = action.payload.doc.id;
+            return data;
+         }))
+      );
+      return this.clients;
+   }
+
+   newClient(client: Client) {
+      this.clientsCollection.add(client);
+   }
+}
