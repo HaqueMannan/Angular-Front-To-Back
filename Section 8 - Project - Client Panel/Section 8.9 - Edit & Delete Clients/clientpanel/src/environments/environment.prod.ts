716423682723fd5eb5f4b5c37599963e24bd4667
Haque <34745319+HaqueMannan@users.noreply.github.com>
2019-01-18 23:29:12 +0000

    Section 8 - Client Panel Project - 8.9. Edit & Delete Clients.

---
+++ Section 8 - Project - Client Panel/Section 8.9 - Edit & Delete Clients/clientpanel/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+   production: true,
+   firebase: {
+      apiKey: "AIzaSyDCWT_5rVJN4Zt8q8OepyM-m8qFII2GVXA",
+      authDomain: "angularclientpanel-cb459.firebaseapp.com",
+      databaseURL: "https://angularclientpanel-cb459.firebaseio.com",
+      projectId: "angularclientpanel-cb459",
+      storageBucket: "angularclientpanel-cb459.appspot.com",
+      messagingSenderId: "744988607953"
+   }
+};
