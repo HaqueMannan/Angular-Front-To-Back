d5171839e129234ef6f881b47ac41ccbfa8ce765
Haque <34745319+HaqueMannan@users.noreply.github.com>
2019-01-19 23:08:46 +0000

    Section 8 - Client Panel Project - 8.11. Logout & Nav Links.

---
+++ Section 8 - Project - Client Panel/Section 8.11 - Logout & Nav Links/clientpanel/src/app/services/auth.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+
+@Injectable({
+   providedIn: 'root'
+})
+export class AuthService {
+
+   constructor(private _afAuth: AngularFireAuth) { }
+
+   login(email: string, password: string) {
+      return new Promise((resolve, reject) => {
+         this._afAuth.auth.signInWithEmailAndPassword(email, password)
+            .then(userData => resolve(userData), err => reject(err));
+      });
+   }
+
+   getAuth() {
+      return this._afAuth.authState.pipe(auth => auth);
+   }
+
+   logout() {
+      this._afAuth.auth.signOut();
+   }
+}
